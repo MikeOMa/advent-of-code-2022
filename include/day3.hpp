c7f6dde989305687d8db8c691ebf626ff27b9bee
MikeOMa <michael.omalley1011@gmail.com>
2022-12-03 18:35:39 +0000

    initial commit for first 3 days

---
+++ include/day3.hpp
@@ -0,0 +1,95 @@
+#ifndef DBMW_DAY3_HPP_
+#define DBMW_DAY3_HPP_
+
+#include"util.hpp"
+#include<vector>
+#include<string>
+#include<iostream>
+#include <bits/stdc++.h>
+
+
+struct day3 {
+    vector<std::set<char>> A;
+    vector<std::set<char>> firstRucksack;
+    vector<std::set<char>> secondRucksack;
+
+    unsigned char2Score(char x){
+        unsigned out=-1;
+        if (isupper(x)){
+            out = x-'A'+27;
+        }else
+            out = x-'a'+1;
+        return out;
+    }
+
+    day3(const std::string &st = "input/input3") {
+        for (auto s: fiterator(st, '\n')) {
+            if (s != "") {
+                set<char> full_sack;
+                for (unsigned i=0; i<s.length(); i++) {
+                    full_sack.insert(s[i]);
+                }
+                A.push_back(full_sack);
+                unsigned N = s.length()/2;
+                set<char> compartment1;
+                set<char> compartment2;
+                for (unsigned i=0; i<N; i++){
+                    compartment1.insert(s[i]);
+                    compartment2.insert(s[i+N]);
+                }
+
+                firstRucksack.push_back(compartment1);
+                secondRucksack.push_back(compartment2);
+            }
+        }
+    }
+
+
+
+
+
+        void firstStar() {
+        unsigned totalScore = 0;
+            for (unsigned e = 0; e < A.size(); e++) {
+                auto compartment1 = firstRucksack[e];
+                auto compartment2 = secondRucksack[e];
+                set<char> intersect;
+                std::set_intersection(compartment1.begin(),
+                                    compartment1.end(),
+                                    compartment2.begin(),
+                                    compartment2.end(),
+                                    std::inserter(intersect, intersect.begin()));
+                for (auto i: intersect){
+                    totalScore+=char2Score(i);
+                }
+            }
+            std::cout << "Third day of Christmas: " << totalScore << std::endl;
+        }
+
+
+        void secondStar() {
+
+            unsigned totalScore = 0;
+            for (unsigned e = 0; e < A.size(); e+=3) {
+                std::set<char> set1 = A[e];
+                for (unsigned row = 1; row < 3; row++) {
+                    auto set2 = A[e+row];
+                    std::set<char> intersect;
+                    std::set_intersection(set1.begin(),
+                                          set1.end(),
+                                          set2.begin(),
+                                          set2.end(),
+                                          std::inserter(intersect, intersect.begin()));
+                    set1 = intersect;
+                }
+
+                for (auto i: set1){
+                    totalScore+=char2Score(i);
+                }
+            }
+            std::cout << "Third Day of Christmas: " << totalScore << std::endl;
+        }
+};
+
+
+#endif
---
+++ include/util.hpp
@@ -0,0 +1,36 @@
+#ifndef DBMW_INCLUDE_UTIL_HPP_
+#define DBMW_INCLUDE_UTIL_HPP_
+
+#include<fstream>
+#include<string>
+#include<vector>
+#include<functional>
+#include<map>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+struct fiterator{
+
+	struct _iter{
+		fiterator *src;
+
+		std::string operator*() const;
+		_iter& operator++();
+		bool operator!=(const _iter& other) const;
+	};
+
+	fiterator(const std::string &filename,const char& separator=' ');
+
+	_iter begin();
+	_iter end();
+	std::string next();
+
+	char sep;
+	std::ifstream f;
+};
+
+std::vector<std::string> tokenize(const std::string &st,std::vector<char> separators={' '});
+
+#endif
