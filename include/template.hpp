e462c8fdcc983ebc2ca9b45522f72035b54f56ed
MikeOMa <michael.omalley1011@gmail.com>
2022-12-04 10:03:12 +0000

    day4

---
+++ include/template.hpp
@@ -0,0 +1,41 @@
+#ifndef DBMW_DAY3_HPP_
+#define DBMW_DAY3_HPP_
+
+#include"util.hpp"
+#include<vector>
+#include<string>
+#include<iostream>
+#include <bits/stdc++.h>
+
+
+struct day3 {
+    vector<std::set<char>> A;
+    vector<std::set<char>> firstRucksack;
+    vector<std::set<char>> secondRucksack;
+
+
+    day3(const std::string &st = "input/input3") {
+        for (auto s: fiterator(st, '\n')) {
+            if (s != "") {
+            }
+        }
+    }
+
+
+
+
+
+        void firstStar() {
+        unsigned totalScore = 0;
+        std::cout << "Third day of Christmas: " << totalScore << std::endl;
+        }
+
+
+        void secondStar() {
+            unsigned totalScore = 0;
+            std::cout << "Third Day of Christmas: " << totalScore << std::endl;
+        }
+};
+
+
+#endif
